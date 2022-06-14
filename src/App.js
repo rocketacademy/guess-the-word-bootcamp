f68d8368da20dcebd00ed4fc1efe0e0890930399
weiyu95 <weiyu95@gmail.com>
2022-06-14 21:24:25 +0800

    added guessedword to initial state and able to generate guessedword

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ class App extends React.Component {
   constructor(props) {
     // Always call super with props in constructor to initialise parent class
     super(props);
-    this.state = {
+    const currWord = getRandomWord();
+    this.initialState = {
       // currWord is the current secret word for this round. Update this with this.setState after each round.
-      currWord: getRandomWord(),
+      currWord: currWord,
       // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
-      // Insert num guesses left state here
-      // Insert form input state here
+      attemptsLeft: 10,
+      currInputWord: "",
+      numOfRoundPlayed: 0,
+      numOfWordsGuessCorrect: 0,
+      guessedWord: "",
     };
+    this.state = { ...this.initialState };
   }
 
   generateWordDisplay = () => {
@@ -30,6 +35,66 @@ class App extends React.Component {
   };
 
   // Insert form callback functions handleChange and handleSubmit here
+  handleChange = (event) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        currInputWord: event.target.value,
+      };
+    });
+    return;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newWord = getRandomWord();
+
+    if (this.state.attemptsLeft === 0) {
+      this.setState({
+        ...this.initialState,
+        currWord: newWord,
+        numOfRoundPlayed: this.state.numOfRoundPlayed + 1,
+        numOfWordsGuessCorrect: this.state.numOfWordsGuessCorrect,
+      });
+      return;
+    } else if (this.state.guessedWord === this.state.currWord) {
+      this.setState({
+        ...this.initialState,
+        currWord: newWord,
+        numOfRoundPlayed: this.state.numOfRoundPlayed + 1,
+        numOfWordsGuessCorrect: this.state.numOfWordsGuessCorrect + 1,
+      });
+      return;
+    }
+
+    if (this.state.currWord.includes(this.state.currInputWord)) {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          currInputWord: "",
+          guessedLetters: [
+            ...prevState.guessedLetters,
+            this.state.currInputWord,
+          ],
+          guessedWord: this.generateWordDisplay().replace(/,/g, ""),
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          currInputWord: "",
+          attemptsLeft: prevState.attemptsLeft - 1,
+          guessedLetters: [
+            ...prevState.guessedLetters,
+            this.state.currInputWord,
+          ],
+        };
+      });
+    }
+
+    return;
+  };
 
   render() {
     return (
@@ -44,7 +109,18 @@ class App extends React.Component {
             : "-"}
           <h3>Input</h3>
           {/* Insert form element here */}
-          Todo: Insert form element here
+          <form onSubmit={this.handleSubmit}>
+            <label>Make a guess</label>
+            <div>
+              <input
+                type="text"
+                value={this.state.currInputWord}
+                onChange={this.handleChange}
+                maxLength={1}
+              ></input>
+            </div>
+            <input type="submit" value="submit" />
+          </form>
         </header>
       </div>
     );
@@ -52,3 +128,15 @@ class App extends React.Component {
 }
 
 export default App;
+
+//   if (window.confirm("No more attempt left, restart the game?")) {
+//     const newWord = getRandomWord();
+//     this.setState({
+//       ...this.initialState,
+//       currWord: newWord,
+//     });
+//   } else {
+//     console.log("user decided not to restart the game");
+//   }
+//   return;
+// }
