a4c02897672bc88f065844a2465c36a4c67078ee
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-07-18 21:59:30 +0800

    added in number of guesses and console log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
+const numStartGuesses = 10;
+
 class App extends React.Component {
   constructor(props) {
     // Always call super with props in constructor to initialise parent class
@@ -12,17 +14,21 @@ class App extends React.Component {
       // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
       // Insert num guesses left state here
+      numGuessesLeft: numStartGuesses,
       // Insert form input state here
       currUserInputCharacter: "",
     };
-    // binding the scope of 'this' to the function 'handleChange'
+    // why is it that if i console log this.state.currWord here, i'll get two words?
   }
 
   generateWordDisplay = () => {
     const wordDisplay = [];
     // for...of is a string and array iterator that does not use index
     for (let letter of this.state.currWord) {
-      if (this.state.guessedLetters.includes(letter)) {
+      if (
+        this.state.guessedLetters.includes(letter) ||
+        this.state.numGuessesLeft === 0
+      ) {
         wordDisplay.push(letter);
       } else {
         wordDisplay.push("_");
@@ -46,29 +52,32 @@ class App extends React.Component {
   // event refers to the user's actions
   handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!this.state.input) {
+      return;
+    }
+
     this.setState((prevState) => {
       return {
         ...prevState,
         guessedLetters: [
           ...prevState.guessedLetters,
-          prevState.currUserInputCharacter,
+          prevState.currUserInputCharacter.toLowerCase(),
         ],
         currUserInputCharacter: "",
+        numGuessesLeft: prevState.numGuessesLeft - 1,
+        input: "",
       };
     });
   };
 
-  // handleChange(event) {
-  //   this.setState({ value: event.target.value });
-  //   event.preventDefault();
-  // }
-
-  // handleSubmit(event) {
-  //   alert("A name was submitted: " + this.state.value);
-  //   event.preventDefault();
-  // }
-
   render() {
+    console.log(
+      "The current word is " +
+        this.state.currWord +
+        " and the number of guesses left is " +
+        this.state.numGuessesLeft
+    );
     return (
       <div className="App">
         <header className="App-header">
@@ -79,8 +88,9 @@ class App extends React.Component {
           {this.state.guessedLetters.length > 0
             ? this.state.guessedLetters.toString()
             : "-"}
+          <h3>You have {this.state.numGuessesLeft} guesses left.</h3>
           <h3>Input</h3>
-          {this.state.currUserInputCharacter}
+          {this.state.currUserInputCharacter.toLowerCase()}
           <form onSubmit={this.handleSubmit}>
             <label>
               Enter letters here:
