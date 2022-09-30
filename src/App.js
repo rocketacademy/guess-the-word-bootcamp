479d2fce07eb948c7f8c1295e4ae52e981d15fe4
mafiageek <anton_kho@msn.com>
2022-09-30 22:39:29 +0800

    Refact Fn Comp

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    // Always call super with props in constructor to initialise parent class
-    super(props);
-    this.state = {
-      // currWord is the current secret word for this round. Update this with this.setState after each round.
-      currWord: getRandomWord(),
-      // guessedLetters stores all letters a user has guessed so far
-      guessedLetters: [],
-      // Insert num guesses left state here
-      // Insert form input state here
-    };
-  }
+export default function App() {
+  const [currWord, setCurrWord] = React.useState(getRandomWord());
+  const [guessedLetters, setGuessedLetters] = React.useState([]);
+  const [numOfGuesses, setNumOfGuesses] = React.useState(0);
+  const [formData, setFormData] = React.useState("");
 
-  generateWordDisplay = () => {
+  const generateWordDisplay = () => {
     const wordDisplay = [];
     // for...of is a string and array iterator that does not use index
-    for (let letter of this.state.currWord) {
-      if (this.state.guessedLetters.includes(letter)) {
+    for (let letter of currWord) {
+      if (guessedLetters.includes(letter)) {
         wordDisplay.push(letter);
       } else {
         wordDisplay.push("_");
@@ -29,26 +21,43 @@ class App extends React.Component {
     return wordDisplay.toString();
   };
 
-  // Insert form callback functions handleChange and handleSubmit here
+  const handleChange = (event) => {
+    setFormData(event.target.value);
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Guess The Word 🚀</h1>
-          <h3>Word Display</h3>
-          {this.generateWordDisplay()}
-          <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
-        </header>
-      </div>
-    );
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setGuessedLetters([...guessedLetters, formData]);
+    setNumOfGuesses((prevGuess) => prevGuess + 1);
+    setFormData((prevFormData) => "");
+  };
+
+  const handleReset = () => {
+    setCurrWord((prevCurrWord) => getRandomWord());
+    setGuessedLetters((prevGuessedLetters) => []);
+    setNumOfGuesses((prevNumOfGuesses) => 0);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Guess The Word 🚀</h1>
+        <h3>Word Display</h3>
+        {generateWordDisplay()}
+        <h3>Guessed Letters</h3>
+        {guessedLetters.length > 0 ? guessedLetters.toString() : "-"}
+        <h3>Input</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="guessedLetter"
+            placeholder="Guessed Letter"
+            onChange={handleChange}
+            value={formData}
+          />
+          <button>Submit</button>
+        </form>
+      </header>
+    </div>
+  );
 }
-
-export default App;
