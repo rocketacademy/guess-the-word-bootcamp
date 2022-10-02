97a31aa40536f76836278822f843f3b10ec72183
ameeralih <ameerali.hassanali@gmail.com>
2022-10-02 15:55:58 +0800

    Base game

--- src/App.js
+++ src/App.js
@@ -13,8 +13,21 @@ class App extends React.Component {
       guessedLetters: [],
       // Insert num guesses left state here
       // Insert form input state here
+      currentGuessedLetter: "",
+      turnCount: 10,
+      winStatus: false,
     };
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.generateWordDisplay = this.generateWordDisplay.bind(this);
   }
+
+  guessedLettersDisplay = () => {
+    let guessedLettersString = this.state.guessedLetters
+      .toString()
+      .toUpperCase()
+      .replace(/,/g, "  ");
+    return guessedLettersString;
+  };
 
   generateWordDisplay = () => {
     const wordDisplay = [];
@@ -23,13 +36,84 @@ class App extends React.Component {
       if (this.state.guessedLetters.includes(letter)) {
         wordDisplay.push(letter);
       } else {
-        wordDisplay.push("_");
+        wordDisplay.push("-");
       }
     }
-    return wordDisplay.toString();
+    if (this.state.winStatus === false) {
+      if (!wordDisplay.includes("-")) {
+        this.setState({
+          winStatus: true,
+        });
+      }
+    }
+
+    return wordDisplay.toString().replace(/,/g, "");
   };
 
-  // Insert form callback functions handleChange and handleSubmit here
+  restartGame = () => {
+    this.setState({
+      currWord: getRandomWord(),
+      guessedLetters: [],
+      currentGuessedLetter: "",
+      turnCount: 10,
+      winStatus: false,
+    });
+  };
+
+  restartButton = () => {
+    if (this.state.turnCount === 0 || this.state.winStatus === true) {
+      return <button onClick={(e) => this.restartGame(e)}>Restart</button>;
+    }
+  };
+
+  winLoseDisplay = () => {
+    if (this.state.turnCount === 0) {
+      return `You lose!`;
+    }
+    if (this.state.winStatus === true) {
+      return `You win!`;
+    }
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // for (let letter of this.state.currWord) {
+    //   if (this.state.guessedLetters.includes(letter)) {
+    //     this.setState((state) => ({
+    //       wordDisplay: [...state.wordDisplay, letter],
+    //     }));
+    //   } else {
+    //     this.setState((state) => ({
+    //       wordDisplay: [...state.wordDisplay, "-"],
+    //     }));
+    //   }
+    // }
+    if (this.state.guessedLetters.includes(this.state.currentGuessedLetter)) {
+    } else {
+      if (!this.state.currWord.includes(this.state.currentGuessedLetter)) {
+        this.setState({
+          turnCount: this.state.turnCount - 1,
+        });
+      }
+      this.setState({
+        guessedLetters: [
+          ...this.state.guessedLetters,
+          this.state.currentGuessedLetter,
+        ],
+      });
+
+      this.setState({
+        currentGuessedLetter: "",
+      });
+      console.log(this.state);
+    }
+  }
+
+  handleFormChange(e) {
+    this.setState({
+      currentGuessedLetter: e.target.value.replace(/[^a-z]/gi, ""),
+    });
+  }
 
   render() {
     return (
@@ -38,13 +122,42 @@ class App extends React.Component {
           <h1>Guess The Word 🚀</h1>
           <h3>Word Display</h3>
           {this.generateWordDisplay()}
+          <h3
+            className={
+              this.state.turnCount < 1 || this.state.winStatus === true
+                ? "hidden"
+                : undefined
+            }
+          >
+            Input
+          </h3>
+          <form
+            onSubmit={(e) => this.handleSubmit(e)}
+            className={
+              this.state.turnCount < 1 || this.state.winStatus === true
+                ? "hidden"
+                : undefined
+            }
+          >
+            <input
+              maxLength="1"
+              name="currentGuessedLetter"
+              type="text"
+              value={this.state.currentGuessedLetter}
+              onChange={(e) => this.handleFormChange(e)}
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
           <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
+          <pre>{this.guessedLettersDisplay()}</pre>
+          <h3>Wrong Guesses Left</h3>
+          {this.state.turnCount}
+          <br></br>
+          <br></br>
+          <p style={{ fontSize: "35px" }}>
+            <b>{this.winLoseDisplay()}</b>
+          </p>
+          {this.restartButton()}
         </header>
       </div>
     );
