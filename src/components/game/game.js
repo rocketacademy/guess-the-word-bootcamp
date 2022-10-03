d180324c7230895c1eaa772279f6909b4cd3ab41
LoyChaiEe <celoy001@gmail.com>
2022-10-03 19:52:05 +0800

    edit win condition

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -11,7 +11,7 @@ class Game extends React.Component {
     this.state = {
       currWord: "overcook",
       guessedLetters: [],
-      displayLetters: [], //empty string to fill in the blanks
+      displayLetters: [], 
       numOfAllowedGuess: this.props.guess,
       numOfGuess: 0,
       userGuess: "",
@@ -26,7 +26,7 @@ class Game extends React.Component {
 
   //Update user's guess
   //there will be a lot of temp variable with similar name as state which is used to update the state
-  updateGuess = (userGuess) => {
+  updateGuessAndWins = (userGuess) => {
     //if user was trying to guess the word
     if (userGuess.length === this.state.currWord.length) {
       if (userGuess === this.state.currWord) {
@@ -35,12 +35,6 @@ class Game extends React.Component {
           wins: gamesWon + 1,
           isRoundDone: true,
         });
-        console.log(
-          this.state.isRoundDone,
-          this.state.wins,
-          this.state.numOfAllowedGuess,
-          this.state.rounds
-        );
       } else {
         let guessLeft = this.state.numOfGuess;
         this.setState({
@@ -49,7 +43,7 @@ class Game extends React.Component {
       }
     }
     //User guessing a letter (reject if user's guess length is more than 1)
-    else if(userGuess.length === 1){
+    else if (userGuess.length === 1) {
       let updateGuess = this.state.guessedLetters;
       //check if user has guessed the letter
       if (updateGuess.indexOf(userGuess, 0) >= 0) {
@@ -73,17 +67,42 @@ class Game extends React.Component {
       }
     }
     //rejects
-    else{
-      alert("Please eneter a SINGLE letter or the word you want to guess.")
+    else {
+      alert("Please eneter a SINGLE letter or the word you want to guess.");
     }
+
+    //Update win condition (letters)
+    const letterToWin = new Set(...this.state.currWord)
+    if(this.state.numOfGuess === this.state.numOfAllowedGuess || letterToWin.length === this.state.displayLetters.length){
+      let win = this.state.wins
+      //loss
+      if(this.state.numOfGuess === this.state.numOfAllowedGuess){
+        this.setState({
+          isRoundDone: true,
+        });
+      }
+      //win
+      else if (letterToWin.length === this.state.displayLetters.length){
+        this.setState({
+          isRoundDone: true,
+          wins: win + 1,
+        });
+      }
+      else{
+        console.log("I don't know how you managed to get to here.")
+      }
+    }
+
   };
 
   render() {
+    const button = <p></p> //placeholder
     return (
       <div>
         <Score score={this.state.wins} total={this.state.currRound} />
         <Display word={this.state.currWord} display={this.state.displayLetters} />
         <Input getGuess={this.updateGuess} />
+        {this.state.isRoundDone && button}
         <Hangman numGuess={this.state.numOfAllowedGuess} guessLeft={this.state.numOfGuess}/>
       </div>
     );
