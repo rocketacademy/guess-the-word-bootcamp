6380ec203947e897d3451ea38698133ded04f02f
daniellimss <highdawn01@yahoo.com>
2023-04-05 15:11:37 +0800

    basic bootstrap design added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
+
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+ 
 class App extends React.Component {
   constructor(props) {
     // Always call super with props in constructor to initialise parent class
@@ -66,6 +70,19 @@ class App extends React.Component {
     }) */
   }
 
+  checkUserGuessedWord = (inputLetter) =>{
+    // create spread array of guessedLetters without changing this.state.guessLetters
+    const guessedLetters = [...this.state.guessedLetters, inputLetter];
+    //loop through to scan whether letter appears inside currWord
+    for(let letter of this.state.currWord){
+      if(!guessedLetters.includes(letter)){
+        return false;
+      }
+    }
+    //Return true if guessedLetters contains all letters inside this.state.currWord
+    return true;
+  };    
+
   resetGame = () =>{
     this.setState({
       currWord: getRandomWord(),
@@ -75,38 +92,60 @@ class App extends React.Component {
     })
   }
 
-
-
-
-
   render() {
+    //Check whether user has guessed correctly the word
+    const hasUserGuessedWord = this.checkUserGuessedWord();
+    const shouldDisableInput = hasUserGuessedWord || this.state.numGuessLeft === 0;
+    const playAgainButton = (
+      <button onClick={this.resetGame}>Play Again?</button>
+    );
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Guess The Word 🚀</h1>
+          <Alert key="primary" variant="primary"><h1>Guess The Word 🚀</h1></Alert>
           <h3>Word Display</h3>
           {this.generateWordDisplay()}
           <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
+          <Alert variant="info">{this.state.guessedLetters.length > 0
             ? this.state.guessedLetters.toString()
-            : "-"}
+            : "-"}</Alert>
+          <p>Num guesses left: <Alert variant="danger">{this.state.numGuessLeft}</Alert></p>
           <h3>Input</h3>
+          <p>Please submit 1 letter at a time.</p>
           {/* Insert form element here */}
           <form onSubmit={this.handleSubmit}>
             <label>
               Letter:
             </label>
-            <input 
+             
+              <input 
               type="text" 
               name="letter"
               value={this.state.letter} 
               onChange={this.handleChange}
-              placeholder="Type in single letter here">
+              placeholder="Type in single letter here"
+              disabled={shouldDisableInput}
+              >
             </input>
+            
             <br />
-            <button type='submit'>Submit Letter</button>
+            <Button variant="primary" type='submit'>Submit Letter</Button>
           </form>
-          Todo: Insert form element here
+           {/* Show congratz message if user guesses word correctly*/}
+            {hasUserGuessedWord && (
+              <div>
+                <p>Correct! You've guessed the word!</p>
+                {playAgainButton}
+              </div>
+            )}
+            {/*Show msg if user runs out of guesses */}
+            {this.state.numGuessLeft === 0 && !hasUserGuessedWord && (
+              <div>
+                <p>Sorry, you ran out of guesses.</p>
+                {playAgainButton}
+              </div>
+            ) }  
+
         </header>
       </div>
     );
