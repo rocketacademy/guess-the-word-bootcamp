73afea1f1b818c477d8b51665e5f9464378bd7f7
calebcianc <calebcc.castro@gmail.com>
2023-04-16 17:24:46 +0800

    Changed the comma in the displayed gussed characters to a space, and changed the placeholder from a hypen to square brackets

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
           <h3>Word Display</h3>
           {this.generateWordDisplay()}
           <h3>Guessed Letters</h3>
-          {guessedLetters.length > 0 ? guessedLetters.toString() : "-"}
+          {guessedLetters.length > 0 ? guessedLetters.join(" ") : "[ ]"}
           {wordDisplayed.includes("_") ? (
             numGuessLeft === 0 ? (
               <div>
