8adbcfe9887257770edcc07bb68e58237538914e
NTU_COSMO <yuhao.huang@ntu.edu.sg>
2023-04-21 14:11:53 +0800

    final version

--- src/DisplayImages.js
+++ src/DisplayImages.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//Class for rendering of images to page based on number of correct and wrong guesses
 export default class DisplayImages extends React.Component {
   constructor(props) {
     // Always call super with props in constructor to initialise parent class
