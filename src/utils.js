e378a2f813c6f23dc7ca7c7dae11ca1f1035bfe6
chengwaikoo <chengwai.koo@gmail.com>
2023-07-29 14:19:48 +0800

    feat: change generated words to uppercase

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,5 @@ import words from "./words.json";
 
 export const getRandomWord = () => {
   // Lowercase words for simplicity
-  return words[Math.floor(Math.random() * words.length)].toLowerCase();
+  return words[Math.floor(Math.random() * words.length)];
 };
