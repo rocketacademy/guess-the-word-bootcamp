77b69252cc72425e5b22c252d31a7ec617afa35d
jewc <jamescew@gmail.com>
2023-07-29 13:49:08 +0800

    Change from class based to functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    // Always call super with props in constructor to initialise parent class
-    super(props);
-    this.state = {
-      // currWord is the current secret word for this round. Update this with this.setState after each round.
-      currWord: getRandomWord(),
-      // guessedLetters stores all letters a user has guessed so far
-      guessedLetters: [],
-      // Insert num guesses left state here
-      // Insert form input state here
-    };
-  }
+const NUM_INITIAL_GUESSES = 6;
 
-  generateWordDisplay = () => {
+const App = () => {
+  const [currWord, setCurrWord] = useState(getRandomWord());
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [numGuessesLeft, setNumGuessesLeft] = useState(NUM_INITIAL_GUESSES);
+  const [input, setInput] = useState("");
+  console.log("Start Curr word:", currWord);
+
+  const resetGame = () => {
+    setCurrWord(getRandomWord());
+    setGuessedLetters([]);
+    setNumGuessesLeft(NUM_INITIAL_GUESSES);
+    setInput("");
+    console.log("Reset game. Curr word", currWord);
+  };
+
+  const generateWordDisplay = () => {
     const wordDisplay = [];
     // for...of is a string and array iterator that does not use index
-    for (let letter of this.state.currWord) {
-      if (this.state.guessedLetters.includes(letter)) {
+    for (let letter of currWord) {
+      if (guessedLetters.includes(letter)) {
         wordDisplay.push(letter);
       } else {
         wordDisplay.push("_");
@@ -30,25 +33,20 @@ class App extends React.Component {
   };
 
   // Insert form callback functions handleChange and handleSubmit here
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Guess The Word 🚀</h1>
-          <h3>Word Display</h3>
-          {this.generateWordDisplay()}
-          <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Guess The Word 🚀</h1>
+        <h3>Word Display</h3>
+        {generateWordDisplay()}
+        <h3>Guessed Letters</h3>
+        {guessedLetters.length > 0 ? guessedLetters.toString() : "-"}
+        <h3>Input</h3>
+        {/* Insert form element here */}
+        Todo: Insert form element here
+      </header>
+    </div>
+  );
+};
 
 export default App;
