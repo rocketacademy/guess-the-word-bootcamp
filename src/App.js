b754d8318acb1e26f33ca8fb82daecaeeddddccc
ianlauyin <ianlauyin@gmail.com>
2023-10-24 23:51:15 +0800

    det Result and display added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
+import detResult from "./detResult.js";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,11 +9,12 @@ class App extends React.Component {
     super(props);
     this.state = {
       // currWord is the current secret word for this round. Update this with this.setState after each round.
-      currWord: getRandomWord(),
+      currWord: "hello",
       // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
       numGuess: 10,
       userWord: "",
+      result: "",
     };
   }
 
@@ -20,16 +22,30 @@ class App extends React.Component {
     const wordDisplay = [];
 
     for (let letter of this.state.currWord) {
-      if (
-        this.state.guessedLetters.includes(letter) ||
-        this.state.numGuess === 0
-      ) {
+      if (this.state.guessedLetters.includes(letter)) {
         wordDisplay.push(letter);
       } else {
         wordDisplay.push("_");
       }
     }
     return wordDisplay.toString();
+  };
+
+  genResultDisplay = () => {
+    if (this.state.result === "win") {
+      return (
+        <h3>Congratulations, You win with {10 - this.state.numGuess} guess.</h3>
+      );
+    }
+    if (this.state.result === "lose") {
+      return <h3>Oh, You have no guess chance left. You lose.</h3>;
+    }
+
+    return (
+      <h3>
+        Error01 :error is occur, please contact the creator for more detail.
+      </h3>
+    );
   };
 
   handleChange = (e) => {
@@ -40,10 +56,10 @@ class App extends React.Component {
   handleSubmit = (e) => {
     let { userWord } = this.state;
     e.preventDefault();
+    this.state.numGuess -= 1;
     this.setState({
       userWord: "",
       guessedLetters: [...this.state.guessedLetters, userWord],
-      numGuess: this.state.numGuess - 1,
     });
   };
 
@@ -61,19 +77,36 @@ class App extends React.Component {
       </form>
     );
 
+    this.state.result = detResult(
+      this.state.currWord,
+      this.state.guessedLetters,
+      this.state.numGuess
+    );
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Guess The Word 🚀</h1>
+
           <h3>Word Display</h3>
           {this.generateWordDisplay()}
           <h3>Guessed Letters</h3>
           {this.state.guessedLetters.length > 0
             ? this.state.guessedLetters.toString()
             : "-"}
-          <h3>Guess letter left: {this.state.numGuess}</h3>
-          <h3>Input</h3>
-          {inputGuess}
+          {this.state.result === "" ? (
+            <div>
+              <h3>Guess letter left: {this.state.numGuess}</h3>
+              <h3>Input</h3>
+              {inputGuess}
+            </div>
+          ) : (
+            <div>
+              {this.genResultDisplay()}
+              <button>Replay</button>
+              <button onClick={window.close}>Quit</button>
+            </div>
+          )}
         </header>
       </div>
     );
