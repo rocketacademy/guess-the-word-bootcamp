ecf7fc968e73947fa1b0dabb22d538ac83300963
ianlauyin <ianlauyin@gmail.com>
2023-10-24 21:13:58 +0800

    guess word with guess number count completed

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
       currWord: getRandomWord(),
       // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
-      // Insert num guesses left state here
-      // Insert form input state here
+      numGuess: 10,
+      userWord: "",
     };
   }
 
@@ -29,9 +29,37 @@ class App extends React.Component {
     return wordDisplay.toString();
   };
 
-  // Insert form callback functions handleChange and handleSubmit here
+  handleChange = (e) => {
+    let { value } = e.target;
+    this.setState({ userWord: value });
+  };
+
+  handleSubmit = (e) => {
+    let { userWord } = this.state;
+    e.preventDefault();
+    this.setState({
+      userWord: "",
+      guessedLetters: [...this.state.guessedLetters, userWord],
+      numGuess: this.state.numGuess - 1,
+    });
+  };
+
+  // and handleSubmit here
 
   render() {
+    const inputGuess = (
+      <form onSubmit={this.handleSubmit}>
+        <label></label>
+        <input
+          type="text"
+          value={this.state.userWord}
+          onChange={this.handleChange}
+          maxLength={1}
+        />
+        <input type="submit" value="Guess!" />
+      </form>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
@@ -42,9 +70,9 @@ class App extends React.Component {
           {this.state.guessedLetters.length > 0
             ? this.state.guessedLetters.toString()
             : "-"}
+          <h3>Guess letter left: {this.state.numGuess}</h3>
           <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
+          {inputGuess}
         </header>
       </div>
     );
